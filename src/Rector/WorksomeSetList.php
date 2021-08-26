6c1c4d369a699ae5eb832c2d20873ac420eb903b
Oliver Nybroe <oliver@worksome.com>
2021-08-26 14:37:14 +0200

    Fix worksome set list path

--- src/Rector/WorksomeSetList.php
+++ src/Rector/WorksomeSetList.php
@@ -6,5 +6,5 @@ use Rector\Set\Contract\SetListInterface;
 
 class WorksomeSetList implements SetListInterface
 {
-    public const LARAVEL_CODE_QUALITY = __DIR__ . '../../config/sets/laravel-code-quality.php';
+    public const LARAVEL_CODE_QUALITY = __DIR__ . '/config/sets/laravel-code-quality.php';
 }
