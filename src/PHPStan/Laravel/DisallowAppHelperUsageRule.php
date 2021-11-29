09aba52de805c1270b76027456b5397a96818e7d
Oliver Nybroe <oliver@worksome.com>
2021-11-29 13:09:50 +0100

    feat(laravel): Disallow app helper usage

---
+++ src/PHPStan/Laravel/DisallowAppHelperUsageRule.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Worksome\CodingStyle\PHPStan\Laravel;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\FuncCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+
+/**
+ * @implements Rule<FuncCall>
+ */
+class DisallowAppHelperUsageRule implements Rule
+{
+    const FUNCTION_NAME = 'app';
+
+    public function getNodeType(): string
+    {
+        return FuncCall::class;
+    }
+
+    /**
+     * @param FuncCall $node
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        $functionName = $node->name->toString();
+
+        if ($functionName !== self::FUNCTION_NAME) {
+            return [];
+        }
+
+        return [
+            RuleErrorBuilder::message(
+                "Usage of app helper is disallowed. Use dependency injection instead."
+            )->build(),
+        ];
+    }
+}
