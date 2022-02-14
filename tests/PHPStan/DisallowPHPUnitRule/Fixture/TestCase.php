79bfec0dd6528d9e7d90c598d07a96b596e568f8
Luke Downing <lukeraymonddowning@gmail.com>
2022-02-14 11:27:38 +0000

    Adds a rule for preventing PHPUnit tests.

---
+++ tests/PHPStan/DisallowPHPUnitRule/Fixture/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Worksome\CodingStyle\Tests\PHPStan\DisallowPHPUnitRule\Fixture;
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+
+}
