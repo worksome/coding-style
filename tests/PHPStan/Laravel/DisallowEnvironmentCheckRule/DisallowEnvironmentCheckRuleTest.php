c360781da998fbf33a8e1ebd4d41df63ecf72c5d
Luke Downing <lukeraymonddowning@gmail.com>
2023-01-04 11:54:37 +0000

    chore(ci): JIRA-6682 Prevent environment checks
    Prevents checking the environment of the application, instead favouring a driver based approach in Worksome codebases.

---
+++ tests/PHPStan/Laravel/DisallowEnvironmentCheckRule/DisallowEnvironmentCheckRuleTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use Worksome\CodingStyle\PHPStan\Laravel\DisallowEnvironmentCheck\DisallowEnvironmentCheckRule;
+
+it('checks for environment checks made using the app helper', function () {
+    $this->rule = new DisallowEnvironmentCheckRule();
+
+    $errors = array_map(fn (int $lineNumber) => [
+        'Environment checks are disallowed. Please use the driver pattern instead.',
+        $lineNumber,
+    ], [4, 5, 6]);
+
+    expect(__DIR__ . "/Fixture/app_helper_environment_checks.php.inc")->toHaveRuleErrors($errors);
+});
+
+it('checks for environment checks made using the app facade', function () {
+    $this->rule = new DisallowEnvironmentCheckRule();
+
+    $errors = array_map(fn (int $lineNumber) => [
+        'Environment checks are disallowed. Please use the driver pattern instead.',
+        $lineNumber,
+    ], [4, 5, 6, 9, 10, 11]);
+
+    expect(__DIR__ . "/Fixture/app_facade_environment_checks.php.inc")->toHaveRuleErrors($errors);
+});
+
+it('checks for environment checks made using an imported app facade', function () {
+    $this->rule = new DisallowEnvironmentCheckRule();
+
+    $errors = array_map(fn (int $lineNumber) => [
+        'Environment checks are disallowed. Please use the driver pattern instead.',
+        $lineNumber,
+    ], [6, 7, 8]);
+
+    expect(__DIR__ . "/Fixture/imported_app_facade_environment_checks.php.inc")->toHaveRuleErrors($errors);
+});
+
+it('checks for environment checks made using the application contract', function () {
+    $this->rule = new DisallowEnvironmentCheckRule();
+
+    $errors = array_map(fn (int $lineNumber) => [
+        'Environment checks are disallowed. Please use the driver pattern instead.',
+        $lineNumber,
+    ], [6, 10]);
+
+    expect(__DIR__ . "/Fixture/application_class_environment_checks.php.inc")->toHaveRuleErrors($errors);
+});
