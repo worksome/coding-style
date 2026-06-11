ee3f33abc7480485c9303956a909fe2b489d72c2
Owen Voke <development@voke.dev>
2026-06-11 14:46:31 +0100

    feat!: JIRA-17366 Update to ECS 13.2
    This updates from ECS 13.1 to ECS 13.2 and refactors the config.

--- ecs.php
+++ ecs.php
@@ -2,15 +2,10 @@
 
 declare(strict_types=1);
 
-use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Worksome\CodingStyle\WorksomeEcsConfig;
 
-
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths([
+return WorksomeEcsConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
-
-    WorksomeEcsConfig::setup($ecsConfig);
-};
