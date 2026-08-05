baf9912391307f04d16833a0279303e4fb582399
Owen Voke <development@voke.dev>
2026-08-05 09:35:08 +0100

    chore(deps-dev): remove Spatie Invade

--- src/Rector/WorksomeSetList.php
+++ src/Rector/WorksomeSetList.php
@@ -4,7 +4,7 @@ namespace Worksome\CodingStyle\Rector;
 
 class WorksomeSetList
 {
-    public const LARAVEL_CODE_QUALITY = __DIR__ . '/config/sets/laravel-code-quality.php';
+    public const string LARAVEL_CODE_QUALITY = __DIR__ . '/config/sets/laravel-code-quality.php';
 
-    public const GENERIC_CODE_QUALITY = __DIR__ . '/config/sets/generic-code-quality.php';
+    public const string GENERIC_CODE_QUALITY = __DIR__ . '/config/sets/generic-code-quality.php';
 }
