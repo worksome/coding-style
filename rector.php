147f6c39ec430b8072244256cced393d00bc146f
Owen Voke <development@voke.dev>
2026-08-05 11:31:04 +0100

    feat: add support for disabling Larastan

--- rector.php
+++ rector.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Worksome\CodingStyle\WorksomeRectorConfig;
 
-return WorksomeRectorConfig::configure()
+return WorksomeRectorConfig::configure(larastan: false)
     ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
